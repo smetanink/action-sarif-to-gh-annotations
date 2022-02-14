85c5765c3c31cc9e1526359be5d80ff8313f4f77
Konstantin Smetanin <konstantin.smetanin@customertimes.com>
2022-02-14 11:09:29 +0300

    Add publish workflow and remove built files.

--- dist/annotations/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { StaticAnalysisResultsFormatSARIFVersion210Rtm4JSONSchema as SchemaV210 } from '../sarif/schemas/sarif-schema-2.1.0';
-export default function createAnnotations(sarif: SchemaV210): Promise<void>;
--- dist/sarif/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { StaticAnalysisResultsFormatSARIFVersion210Rtm4JSONSchema as SchemaV210 } from './schemas/sarif-schema-2.1.0';
-export default function getSarifReport(fileName: string): SchemaV210;
